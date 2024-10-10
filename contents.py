4336cfac8f2452073372a2b342158aac710e9e23
Orzimurod <orzimurodyusupov2004@gmail.com>
2024-10-10 21:04:57 +0500

    register

---
+++ contents.py
@@ -0,0 +1,8 @@
+words = {
+    'name-uz': "Ismingizni kiriting: ",
+    'name-ru': "Введите свое имя: ",
+    'phone-uz': "Numeringizni kiriting:\nMasalan: +998 88 992 1819",
+    'phone-ru': "Введите свой номер:\nНапример: +7 401 ... ",
+    'company-uz': "Kompaniya nomini kiriting:",
+    'company-ru': "Введите название компании:",
+}
--- utils.py
+++ utils.py
@@ -1,19 +1,5 @@
-async def control(name):
-    users = {
-        "Asilbek": 'Darsda bor',
-        "Azizbek": 'Darsda bor',
-        "Jaloliddin": 'Yoq',
-    }
-    try:
-        return str(users[name])
-    except:
-        return "Bunday o'quvchi mavjud emas"
-    
+from contents import words
 
-async def calculate(text):
-    try:
-        a, b = text.split("+")
-        ans = int(a) + int(b)
-        return str(ans)
-    except Exception as e:
-        return f"{e}"
+async def get_word(key, lang = 'uz'):
+    word = words.get(key+ '-' + lang)
+    return word
